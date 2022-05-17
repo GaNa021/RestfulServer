c910385ef76345fed5fa31122e7d81c3e93df6a3
porus <70854290+GaNa021@users.noreply.github.com>
2022-05-17 11:01:32 +0530

    Update Resources.java

--- Resources.java
+++ Resources.java
@@ -1,10 +1,11 @@
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@PATH("/")
+@Path("/")
 public class Resources
 {
 	@GET
